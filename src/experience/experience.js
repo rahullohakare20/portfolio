1c99bce41fce33deb1846f4c794d76267f8b0da9
rahul <rahul.lohakare@varian.com>
2019-03-03 18:14:31 +0530

    portfolio : menu related changes, info changes

--- src/experience/experience.js
+++ src/experience/experience.js
@@ -1,6 +1,7 @@
 import React  from 'react';
 import MenuLogo from '../menuLogo/menuLogo';
 import MainNav from '../mainNav/MainNav';
+import './experience.css'
 
 function Experience(props) {
 
@@ -20,21 +21,26 @@ function Experience(props) {
                                         <h4>Work Experience</h4>
 
                                         <div className="content-details content-border">
-                                            <h5>Mad Max Design</h5>
-                                            <h6>OCT 2013 - june 2014</h6>
-                                            <p>orem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
-                                                nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-                                                volutpat.</p>
+                                            <h5>Varian Medical Systems</h5>
+                                            <h6>July 2018 - March 2019, Pune Maharashtra</h6>
+                                            <p>Worked on varian's Halo 360 product using technologies such as angularJs, angular 6 and React with redux </p>
+                                        </div>
+
+
+                                        <div className="content-details content-border">
+                                            <h5>Xento Systems</h5>
+                                            <h6>October 2015 - July 2018, Pune Maharashtra</h6>
+                                            <p>Worked on various products of xento systems including Message center and Document management</p>
+                                            <p>Worked on transforming front end part into react js</p>
                                         </div>
 
                                         <div className="content-details">
-                                            <h5>Crazy Design</h5>
-                                            <h6>OCT 2013 - june 2015</h6>
-                                            <p>orem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
-                                                nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-                                                volutpat.</p>
+                                            <h5>Futurism Technologies</h5>
+                                            <h6>August 2014 - October 2015, Pune Maharashtra</h6>
+                                            <p>More than 5 client websites built with technology stack of Javascript, JQuery and php.</p>
+                                            <p>Worked on websites including e-commerce, informative, blogs and product portfolios.</p>
+                                            <p>Got star performer 2015</p>
                                         </div>
-
                                     </div>
                                 </div>
 
@@ -44,19 +50,27 @@ function Experience(props) {
                                         <h4>Education</h4>
 
                                         <div className="content-details content-border">
-                                            <h5>Maddison University</h5>
-                                            <h6>OCT 2013 - june 2014</h6>
-                                            <p>orem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
-                                                nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-                                                volutpat.</p>
+                                            <h5>University of Pune</h5>
+                                            <h6>July 2010 - July 2014</h6>
+                                            <p>Bachelor of Engineering in computer engineering from Samanway pratishthan's Institute of knowledge, college of engineering, pune.</p>
                                         </div>
-
+                                        <div className="content-details content-border">
+                                            <h5>Amravati University</h5>
+                                            <h6>July 2008 - July 2010</h6>
+                                            <p>Hsc from sudhakarrao naik college of science, akola.</p>
+                                            <br/>
+                                            <br/>
+                                        </div>
                                         <div className="content-details">
-                                            <h5>Max Design & Art Institute</h5>
-                                            <h6>OCT 2013 - june 2015</h6>
-                                            <p>orem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
-                                                nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-                                                volutpat.</p>
+                                            <h5>Amravati University</h5>
+                                            <h6>July 2007 - July 2008</h6>
+                                            <p>Ssc from Hindu Dnyanpeeth convent, akola.</p>
+                                            <br/>
+                                            <br/>
+                                            <br/>
+                                            <br/>
+                                            <br/>
+                                            <br/>
                                         </div>
 
                                     </div>
--- src/resume/resume.js
+++ src/resume/resume.js
@@ -1,8 +1,8 @@
 import React , {Component} from 'react';
 import MenuLogo from '../menuLogo/menuLogo';
 import MainNav from '../mainNav/MainNav';
-import { Document, Page } from 'react-pdf';
 import Button from 'react-bootstrap/Button'
+import './resume.css';
 
 class Resume extends Component {
     state = {
@@ -24,19 +24,19 @@ class Resume extends Component {
                     <div className="container">
                         <div className="row">
                             <MenuLogo color="grey" {...this.props} />
-                            <div className="about-wrapper">
-                                <div>
-                                    <Document
-                                        file="assets/Rahul-Lohakare-Resume.pdf"
-                                        onLoadSuccess={this.onDocumentLoadSuccess}
-                                    >
-                                        <Page pageNumber={pageNumber} />
-                                    </Document>
-                                    <p>Page {pageNumber} of {numPages}</p>
+                        </div>
+                    </div>
+                    <div className="container">
+                        <div className="row">
+                                <div className="about-wrapper">
+                                    <div>
+                                        <Button href="assets/Rahul-Lohakare-Resume.pdf" className="margin-bottom-10" download variant="success">
+                                            <i className="fa fa-download"></i> Download Resume</Button>
+                                    </div>
+                                    <div>
+                                        <embed src="assets/Rahul-Lohakare-Resume.pdf" width="100%" height="2100px" />
+                                    </div>
                                 </div>
-                                <Button href="assets/Rahul-Lohakare-Resume.pdf" download variant="success">
-                                    <i className="fa fa-download"></i> Download Resume</Button>
-                            </div>
                         </div>
                     </div>
                 </section>
