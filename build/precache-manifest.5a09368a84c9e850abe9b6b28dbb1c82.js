b9fb9d6c17e26734c8d0ca2ef836e96069690381
Rahul Lohakare <rahullohakare20@gmail.com>
2020-09-06 23:13:27 +0530

    committing build

---
+++ build/precache-manifest.5a09368a84c9e850abe9b6b28dbb1c82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb21dd7ff6dff20a25c0",
+    "url": "/static/css/main.3351ae5b.chunk.css"
+  },
+  {
+    "revision": "cb21dd7ff6dff20a25c0",
+    "url": "/static/js/main.cb21dd7f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d54784af4bb62ec4c53a",
+    "url": "/static/js/2.d54784af.chunk.js"
+  },
+  {
+    "revision": "046bfb634514e3eb76951752bf675be3",
+    "url": "/index.html"
+  }
+];
